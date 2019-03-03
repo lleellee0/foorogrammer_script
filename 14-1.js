690666d609bde70e2c97d2cab23ab9fc2d46798a
lleellee0 <lleellee013@gmail.com>
2019-03-03 21:39:05 +0900

    14편

---
+++ 14-1.js
@@ -0,0 +1,31 @@
+1. Timestamp
+  - 1551613324
+  - https://www.unixtimestamp.com/
+
+2. Hash
+  - md5 128비트
+	5D41402ABC4B2A76B9719D911017C592
+
+  - Sha-1
+	AAF4C61DDCC5E8A2DABEDE0F3B482CD9AEA9434D
+
+  - Sha-256
+	2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824
+
+  - 그 외 매우많음..
+
+3. 16진수
+  - 0xABCD
+
+4. BASE64
+  - A~Z, a~z, 0~9, +, /, =(비어있음.)
+  - https://ko.wikipedia.org/wiki/%EB%B2%A0%EC%9D%B4%EC%8A%A464
+	https://ko.wikipedia.org/wiki/%EB%B2%A0%EC%9D%B4%EC%8A%A4
+  - BASE58도 있음. 123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz
+    0 (zero), O (capital o), I (capital i) and l (lower case L) as well as the non-alphanumeric characters + (plus) and / (slash)가 빠짐
+
+5. Percent Encoding
+  - https://search.naver.com/search.naver?ie=UTF-8&query=%EC%95%88%EB%85%95&sm=chr_hty
+
+6. 유니코드들
+  - https://www.branah.com/unicode-converter
