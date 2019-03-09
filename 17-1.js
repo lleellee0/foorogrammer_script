0ed2dd20dad398ad9cf3722a592ccf628f6d6181
lleellee0 <lleellee013@gmail.com>
2019-03-09 22:28:11 +0900

    17화 추가

---
+++ 17-1.js
@@ -0,0 +1,10 @@
+
+// 웹툰 메인 URL 입력 => 해당 웹툰의 모든 화 URL(ID 얻기)
+// http://webtoon.daum.net/data/pc/webtoon/view/{웹툰이름}
+// obj.data.webtoon.webtoonEpisodes[i].id
+
+// 각 화의 URL 입력 => 해당 화의 모든 웹툰 이미지 URL
+// http://webtoon.daum.net/data/pc/webtoon/viewer_images/{각화의 id}
+// obj.data[i].url
+
+// 해당 화의 모든 웹툰 이미지 URL => 파일로 다운로드
